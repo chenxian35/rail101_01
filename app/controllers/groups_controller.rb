41179885018167b0b2ffdd9583e9b279cb195bcb
chenxian35 <linwzh32@gmail.com>
2017-03-23 08:09:02 +0800

    add order by created_at to group's post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,6 +9,8 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def show
   @group = Group.find(params[:id])
+  #@posts = @group.posts
+  @posts = @group.posts.order("created_at DESC")
  end
 
   def edit
@@ -43,7 +45,7 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
       def destroy
         @group = Group.find(params[:id])
-         
+
            @group.destroy
           flash[:alert] = "Group deleted"
           redirect_to groups_path
